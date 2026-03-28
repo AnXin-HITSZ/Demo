8ba8ba483eb59bd0282c3886aa020b6a977c04b4
AnXin-HITSZ <anxin.hitsz@outlook.com>
2026-03-28 20:48:52 +0800

    src - 20260328

---
+++ Python/Python-Project/py_project01/第一章/01.入门程序.py
@@ -0,0 +1,4 @@
+# 需求：往控制台中输出“Hello World”、“Hello Python”（# Python 中注释的写法，注释是给开发人员看的，Python 解释器会忽略注释 -- 快捷键：Ctrl + /）
+print("Hello World")
+print("Hello Python")
+print("----------------------------------")
